00122f0b073a8e74f7f4348197619c4c5dcdca71
William Trevor Olson <wtolson@gmail.com>
2013-05-13 16:35:10 -0700

    Use connection pools to make connections thread safe.

--- pybloomd.py
+++ pybloomd.py
@@ -3,6 +3,7 @@ This module implements a client for the BloomD server.
 """
 __all__ = ["BloomdError", "BloomdConnection", "BloomdClient", "BloomdFilter"]
 __version__ = "0.4.1"
+import os
 import logging
 import socket
 import errno
@@ -17,7 +18,7 @@ class BloomdError(Exception):
 
 class BloomdConnection(object):
     "Provides a convenient interface to server connections"
-    def __init__(self, server, timeout, attempts=3):
+    def __init__(self, server, timeout, attempts=3, pool=None):
         """
         Creates a new Bloomd Connection.
 
@@ -27,6 +28,8 @@ class BloomdConnection(object):
             - timeout: The socket timeout to use.
             - attempts (optional): Maximum retry attempts on errors. Defaults to 3.
         """
+        self.pid = os.getpid()
+
         # Parse the host/port
         parts = server.split(":", 1)
         if len(parts) == 2:
@@ -39,6 +42,7 @@ class BloomdConnection(object):
         self.sock = None
         self.fh = None
         self.attempts = attempts
+        self.pool = pool
         self.logger = logging.getLogger("pybloomd.BloomdConnection.%s.%d" % self.server)
 
     def _create_socket(self):
@@ -129,6 +133,77 @@ class BloomdConnection(object):
         resp_lines = self.readblock()
         return dict(tuple(l.split(" ", 1)) for l in resp_lines)
 
+    def disconnect(self):
+        "Disconnects from the Bloomd server"
+        if self.sock is None:
+            return
+        try:
+            self.sock.close()
+        except socket.error:
+            pass
+        self.sock = None
+
+    def release(self):
+        if self.pool is None:
+            return
+        self.pool.release(self)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release()
+
+
+class ConnectionPool(object):
+    "Generic connection pool"
+    def __init__(self, connection_class=BloomdConnection, max_connections=None,
+                 **connection_kwargs):
+        self.pid = os.getpid()
+        self.connection_class = connection_class
+        self.connection_kwargs = connection_kwargs
+        self.max_connections = max_connections or 2 ** 31
+        self._created_connections = 0
+        self._available_connections = []
+        self._in_use_connections = set()
+
+    def _checkpid(self):
+        if self.pid != os.getpid():
+            self.disconnect()
+            self.__init__(self.connection_class, self.max_connections,
+                          **self.connection_kwargs)
+
+    def get_connection(self):
+        "Get a connection from the pool"
+        self._checkpid()
+        try:
+            connection = self._available_connections.pop()
+        except IndexError:
+            connection = self.make_connection()
+        self._in_use_connections.add(connection)
+        return connection
+
+    def make_connection(self):
+        "Create a new connection"
+        if self._created_connections >= self.max_connections:
+            raise ConnectionError("Too many connections")
+        self._created_connections += 1
+        return self.connection_class(pool=self, **self.connection_kwargs)
+
+    def release(self, connection):
+        "Releases the connection back to the pool"
+        self._checkpid()
+        if connection.pid == self.pid:
+            self._in_use_connections.remove(connection)
+            self._available_connections.append(connection)
+
+    def disconnect(self):
+        "Disconnects all connections in the pool"
+        all_conns = chain(self._available_connections,
+                          self._in_use_connections)
+        for connection in all_conns:
+            connection.disconnect()
+
 
 class BloomdClient(object):
     "Provides a client abstraction around the BloomD interface."
@@ -145,21 +220,21 @@ class BloomdClient(object):
             raise ValueError("Must provide at least 1 server!")
         self.servers = servers
         self.timeout = timeout
-        self.server_conns = {}
+        self.sever_pools = {}
         self.server_info = None
         self.info_time = 0
         self.hash_keys = hash_keys
 
-    def _server_connection(self, server):
+    def _server_pool(self, server):
         "Returns a connection to a server, tries to cache connections."
-        if server in self.server_conns:
-            return self.server_conns[server]
+        if server in self.sever_pools:
+            return self.sever_pools[server]
         else:
-            conn = BloomdConnection(server, self.timeout)
-            self.server_conns[server] = conn
-            return conn
+            pool = ConnectionPool(server=server, timeout=self.timeout)
+            self.sever_pools[server] = pool
+            return pool
 
-    def _get_connection(self, filter, strict=True, explicit_server=None):
+    def _get_pool(self, filter, strict=True, explicit_server=None):
         """
         Gets a connection to a server which is able to service a filter.
         Because filters may exist on any server, all servers are queried
@@ -182,7 +257,7 @@ class BloomdClient(object):
         # Check if this filter is in a known location
         if filter in self.server_info:
             serv = self.server_info[filter][0]
-            return self._server_connection(serv)
+            return self._server_pool(serv)
 
         # Possibly old data? Reload
         self.server_info = self.list_filters(inc_server=True)
@@ -191,7 +266,7 @@ class BloomdClient(object):
         # Recheck
         if filter in self.server_info:
             serv = self.server_info[filter][0]
-            return self._server_connection(serv)
+            return self._server_pool(serv)
 
         # Check if this is fatal
         if strict:
@@ -199,7 +274,7 @@ class BloomdClient(object):
 
         # We have an explicit server provided to us, use that
         if explicit_server:
-            return self._server_connection(explicit_server)
+            return self._server_pool(explicit_server)
 
         # Does not exist, and is not not strict
         # we can select a server on any criteria then.
@@ -215,7 +290,7 @@ class BloomdClient(object):
 
         # Select the least used
         serv = counts[0][1]
-        return self._server_connection(serv)
+        return self._server_pool(serv)
 
     def create_filter(self, name, capacity=None, prob=None, in_memory=False, server=None):
         """
@@ -235,18 +310,23 @@ class BloomdClient(object):
         """
         if prob and not capacity:
             raise ValueError("Must provide size with probability!")
-        conn = self._get_connection(name, strict=False, explicit_server=server)
-        cmd = "create %s" % name
-        if capacity:
-            cmd += " capacity=%d" % capacity
-        if prob:
-            cmd += " prob=%f" % prob
-        if in_memory:
-            cmd += " in_memory=1"
-        conn.send(cmd)
-        resp = conn.read()
+
+        pool = self._get_pool(name, strict=False, explicit_server=server)
+
+        with pool.get_connection() as conn:
+            cmd = "create %s" % name
+            if capacity:
+                cmd += " capacity=%d" % capacity
+            if prob:
+                cmd += " prob=%f" % prob
+            if in_memory:
+                cmd += " in_memory=1"
+
+            conn.send(cmd)
+            resp = conn.read()
+
         if resp == "Done":
-            return BloomdFilter(conn, name, self.hash_keys)
+            return BloomdFilter(pool, name, self.hash_keys)
         elif resp == "Exists":
             return self[name]
         else:
@@ -254,8 +334,8 @@ class BloomdClient(object):
 
     def __getitem__(self, name):
         "Gets a BloomdFilter object based on the name."
-        conn = self._get_connection(name)
-        return BloomdFilter(conn, name, self.hash_keys)
+        pool = self._get_pool(name)
+        return BloomdFilter(pool, name, self.hash_keys)
 
     def list_filters(self, inc_server=False):
         """
@@ -266,16 +346,20 @@ class BloomdClient(object):
             - inc_server (optional) : If true, the dictionary values
                will be (server, filter_info) instead of filter_info.
         """
-        # Send the list to all first
+        connections = []
         for server in self.servers:
-            conn = self._server_connection(server)
+            pool = self._server_pool(server)
+            connections.append(pool.get_connection())
+
+        # Send the list to all first
+        for conn in connections:
             conn.send("list")
 
         # Check response from all
         responses = {}
-        for server in self.servers:
-            conn = self._server_connection(server)
+        for conn in connections:
             resp = conn.readblock()
+            conn.release()
             for line in resp:
                 name, info = line.split(" ", 1)
                 if inc_server:
@@ -287,31 +371,42 @@ class BloomdClient(object):
 
     def flush(self):
         "Instructs all servers to flush to disk"
-        # Send the flush to all first
+        connections = []
         for server in self.servers:
-            conn = self._server_connection(server)
+            pool = self._server_pool(server)
+            connections.append(pool.get_connection())
+
+        # Send the flush to all first
+        for conn in connections:
             conn.send("flush")
 
         # Check response from all
-        for server in self.servers:
-            conn = self._server_connection(server)
+        error = None
+        for conn in connections:
             resp = conn.read()
-            if resp != "Done":
-                raise BloomdError("Got response: '%s' from '%s'" % (resp, server))
+            conn.release()
+
+            if not error and resp != "Done":
+                args = (resp,) + conn.server
+                msg = "Got response: '%s' from '%s:%s'" % args
+                error = error or BloomdError(msg)
+
+        if error:
+            raise error
 
 
 class BloomdFilter(object):
     "Provides an interface to a single Bloomd filter"
-    def __init__(self, conn, name, hash_keys=False):
+    def __init__(self, pool, name, hash_keys=False):
         """
         Creates a new BloomdFilter object.
 
         :Parameters:
-            - conn : The connection to use
+            - pool : The connection pool to use
             - name : The name of the filter
             - hash_keys : Should the keys be hashed client side
         """
-        self.conn = conn
+        self.pool = pool
         self.name = name
         self.hash_keys = hash_keys
 
@@ -327,7 +422,9 @@ class BloomdFilter(object):
         """
         Adds a new key to the filter. Returns True/False if the key was added.
         """
-        resp = self.conn.send_and_receive("s %s %s" % (self.name, self._get_key(key)))
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive("s %s %s" % (self.name, self._get_key(key)))
+
         if resp in ("Yes", "No"):
             return resp == "Yes"
         raise BloomdError("Got response: %s" % resp)
@@ -335,7 +432,9 @@ class BloomdFilter(object):
     def bulk(self, keys):
         "Performs a bulk set command, adds multiple keys in the filter"
         command = ("b %s " % self.name) + " ".join([self._get_key(k) for k in keys])
-        resp = self.conn.send_and_receive(command)
+
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive(command)
 
         if resp.startswith("Yes") or resp.startswith("No"):
             return [r == "Yes" for r in resp.split(" ")]
@@ -343,7 +442,8 @@ class BloomdFilter(object):
 
     def drop(self):
         "Deletes the filter from the server. This is permanent"
-        resp = self.conn.send_and_receive("drop %s" % (self.name))
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive("drop %s" % (self.name))
         if resp != "Done":
             raise BloomdError("Got response: %s" % resp)
 
@@ -351,7 +451,8 @@ class BloomdFilter(object):
         """
         Closes the filter on the server.
         """
-        resp = self.conn.send_and_receive("close %s" % (self.name))
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive("close %s" % (self.name))
         if resp != "Done":
             raise BloomdError("Got response: %s" % resp)
 
@@ -359,13 +460,15 @@ class BloomdFilter(object):
         """
         Clears the filter on the server.
         """
-        resp = self.conn.send_and_receive("clear %s" % (self.name))
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive("clear %s" % (self.name))
         if resp != "Done":
             raise BloomdError("Got response: %s" % resp)
 
     def __contains__(self, key):
         "Checks if the key is contained in the filter."
-        resp = self.conn.send_and_receive("c %s %s" % (self.name, self._get_key(key)))
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive("c %s %s" % (self.name, self._get_key(key)))
         if resp in ("Yes", "No"):
             return resp == "Yes"
         raise BloomdError("Got response: %s" % resp)
@@ -373,7 +476,9 @@ class BloomdFilter(object):
     def multi(self, keys):
         "Performs a multi command, checks for multiple keys in the filter"
         command = ("m %s " % self.name) + " ".join([self._get_key(k) for k in keys])
-        resp = self.conn.send_and_receive(command)
+
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive(command)
 
         if resp.startswith("Yes") or resp.startswith("No"):
             return [r == "Yes" for r in resp.split(" ")]
@@ -386,32 +491,34 @@ class BloomdFilter(object):
 
     def info(self):
         "Returns the info dictionary about the filter."
-        self.conn.send("info %s" % (self.name))
-        return self.conn.response_block_to_dict()
+        with self.pool.get_connection() as conn:
+            conn.send("info %s" % (self.name))
+            return self.conn.response_block_to_dict()
 
     def flush(self):
         "Forces the filter to flush to disk"
-        resp = self.conn.send_and_receive("flush %s" % (self.name))
+        with self.pool.get_connection() as conn:
+            resp = conn.send_and_receive("flush %s" % (self.name))
         if resp != "Done":
             raise BloomdError("Got response: %s" % resp)
 
     def pipeline(self):
         "Creates a BloomdPipeline for pipelining multiple queries"
-        return BloomdPipeline(self.conn, self.name, self.hash_keys)
+        return BloomdPipeline(self.pool, self.name, self.hash_keys)
 
 
 class BloomdPipeline(object):
     "Provides an interface to a single Bloomd filter"
-    def __init__(self, conn, name, hash_keys=False):
+    def __init__(self, pool, name, hash_keys=False):
         """
         Creates a new BloomdPipeline object.
 
         :Parameters:
-            - conn : The connection to use
+            - pool : The connection pool to use
             - name : The name of the filter
             - hash_keys : Should the keys be hashed client side
         """
-        self.conn = conn
+        self.pool = pool
         self.name = name
         self.hash_keys = hash_keys
         self.buf = []
@@ -491,45 +598,46 @@ class BloomdPipeline(object):
         the server in the order issued, and responses are returned
         in appropriate order.
         """
-        # Send each command
-        buf = self.buf
-        self.buf = []
-        for name, cmd in buf:
-            self.conn.send(cmd)
+        with self.pool.get_connection() as conn:
+            # Send each command
+            buf = self.buf
+            self.buf = []
+            for name, cmd in buf:
+                conn.send(cmd)
 
-        # Get the responses
-        all_resp = []
-        for name, cmd in buf:
-            if name in ("bulk", "multi"):
-                resp = self.conn.read()
-                if resp.startswith("Yes") or resp.startswith("No"):
-                    all_resp.append([r == "Yes" for r in resp.split(" ")])
+            # Get the responses
+            all_resp = []
+            for name, cmd in buf:
+                if name in ("bulk", "multi"):
+                    resp = conn.read()
+                    if resp.startswith("Yes") or resp.startswith("No"):
+                        all_resp.append([r == "Yes" for r in resp.split(" ")])
+                    else:
+                        all_resp.append(BloomdError("Got response: %s" % resp))
+
+                elif name in ("add", "check"):
+                    resp = conn.read()
+                    if resp in ("Yes", "No"):
+                        all_resp.append(resp == "Yes")
+                    else:
+                        all_resp.append(BloomdError("Got response: %s" % resp))
+
+
+                elif name in ("drop", "close", "clear", "flush"):
+                    resp = conn.read()
+                    if resp == "Done":
+                        all_resp.append(True)
+                    else:
+                        all_resp.append(BloomdError("Got response: %s" % resp))
+
+                elif name == "info":
+                    try:
+                        resp = conn.response_block_to_dict()
+                        all_resp.append(resp)
+                    except BloomdError, e:
+                        all_resp.append(e)
                 else:
-                    all_resp.append(BloomdError("Got response: %s" % resp))
+                    raise Exception("Unknown command! Command: %s" % name)
 
-            elif name in ("add", "check"):
-                resp = self.conn.read()
-                if resp in ("Yes", "No"):
-                    all_resp.append(resp == "Yes")
-                else:
-                    all_resp.append(BloomdError("Got response: %s" % resp))
-
-
-            elif name in ("drop", "close", "clear", "flush"):
-                resp = self.conn.read()
-                if resp == "Done":
-                    all_resp.append(True)
-                else:
-                    all_resp.append(BloomdError("Got response: %s" % resp))
-
-            elif name == "info":
-                try:
-                    resp = self.conn.response_block_to_dict()
-                    all_resp.append(resp)
-                except BloomdError, e:
-                    all_resp.append(e)
-            else:
-                raise Exception("Unknown command! Command: %s" % name)
-
-        return all_resp
+            return all_resp
 
